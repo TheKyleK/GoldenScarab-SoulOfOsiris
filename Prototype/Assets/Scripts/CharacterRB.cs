014988677fb183338d33499d5c1ed2f6f116a947
dannyli0109_ <dannyli0109@gmail.com>
2020-09-30 19:18:50 +1000

    = update prefabs

--- Prototype/Assets/Scripts/CharacterRB.cs
+++ Prototype/Assets/Scripts/CharacterRB.cs
@@ -12,14 +12,14 @@ public class CharacterRB : MonoBehaviour
     public float speed;
     public CharacterController controller;
 
-    //public bool updateRotation;
+    public bool updateRotation;
     void FixedUpdate()
     {
         ClampMoveSpeed();
         velocity += acceleration * Time.fixedDeltaTime;
         controller.Move(velocity * Time.fixedDeltaTime);
         ApplyFriction();
-        //UpdateRotation();
+        UpdateRotation();
         acceleration *= 0;
     }
 
@@ -43,26 +43,26 @@ public class CharacterRB : MonoBehaviour
     }
 
 
-    //void UpdateRotation()
-    //{
-    //    if (updateRotation)
-    //    {
-    //        if (velocity.magnitude > 0)
-    //        {
-    //            float heading = Mathf.Atan2(velocity.x, velocity.z);
-    //            transform.localRotation = Quaternion.Euler(0, heading * Mathf.Rad2Deg, 0);
-    //        }
-    //        else
-    //        {
-    //            // not stable
-    //            MonsterMovement mm = GetComponent<MonsterMovement>();
-    //            if (mm)
-    //            {
-    //                Vector3 playerPos = mm.player.transform.position;
-    //                float heading = Mathf.Atan2(transform.position.x - playerPos.x, transform.position.z - playerPos.z);
-    //                transform.localRotation = Quaternion.Euler(0, heading * Mathf.Rad2Deg + 180, 0);
-    //            }
-    //        }
-    //    }
-    //}
+    void UpdateRotation()
+    {
+        if (updateRotation)
+        {
+            if (velocity.magnitude > 0)
+            {
+                float heading = Mathf.Atan2(velocity.x, velocity.z);
+                transform.localRotation = Quaternion.Euler(0, heading * Mathf.Rad2Deg, 0);
+            }
+            else
+            {
+                // not stable
+                MonsterMovement mm = GetComponent<MonsterMovement>();
+                if (mm)
+                {
+                    Vector3 playerPos = mm.player.transform.position;
+                    float heading = Mathf.Atan2(transform.position.x - playerPos.x, transform.position.z - playerPos.z);
+                    transform.localRotation = Quaternion.Euler(0, heading * Mathf.Rad2Deg + 180, 0);
+                }
+            }
+        }
+    }
 }
