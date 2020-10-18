e265d9a8c3d02811b33f2e4f4c8608a34289764c
dannyli0109_ <dannyli0109@gmail.com>
2020-10-19 00:35:34 +1100

    = added cast distance y for character movement to deal with gravity

--- Prototype/Assets/CharacterMovement.cs
+++ Prototype/Assets/CharacterMovement.cs
@@ -10,6 +10,7 @@ public class CharacterMovement : MonoBehaviour
     public LayerMask obstacleMask;
     public LayerMask groundMask;
     public float radiusRatio;
+    public float castDistanceY;
     CharacterRB rb;
     // Start is called before the first frame update
     void Start()
@@ -20,12 +21,7 @@ public class CharacterMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Vector3 p1 = transform.position + new Vector3(0, radius, 0);
-        //Vector3 p2 = transform.position + new Vector3(0, radius, 0) + new Vector3(0, height - radius * 2, 0);
-        //Gizmos.DrawWireSphere(p1, radius);
-        //Gizmos.DrawWireSphere(p2, radius);
-        //Vector3 p1 = transform.position + center + Vector3.up * -height * 0.5F;
-        //Vector3 p2 = p1 + Vector3.up * height;
+
     }
 
     public void Move(Vector3 moveVec)
@@ -33,34 +29,10 @@ public class CharacterMovement : MonoBehaviour
         Vector3 p1 = transform.position + Vector3.up * radius + center;
         Vector3 p2 = transform.position + Vector3.up * (height - radius) + center;
 
-        //float dist = moveVec.magnitude;
         RaycastHit hit;
-        //if (rb.isGrounded)
-        //{
-        //    moveVec.y = 0;
-
-        //}
-
-        //hits = Physics.CapsuleCastAll(p1, p2, radius, Vector3.down, 0, groundMask);
-        //foreach(RaycastHit hit in hits)
-        //{
-        //    moveVec.y = 0;
-        //}
-
-        //hits = Physics.CapsuleCastAll(p1, p2, radius, Vector3.Scale(moveVec, Vector3.forward).normalized, 0, obstacleMask);
-        //foreach (RaycastHit hit in hits)
-        //{
-        //    moveVec.z = 0;
-        //}
-
-        //hits = Physics.CapsuleCastAll(p1, p2, radius, Vector3.Scale(moveVec, Vector3.right).normalized, 0, obstacleMask);
-        //foreach (RaycastHit hit in hits)
-        //{
-        //    moveVec.x = 0;
-        //}
 
         Vector3 moveUpward = Vector3.Scale(moveVec, Vector3.up);
-        if (Physics.CapsuleCast(p1, p2, radius * radiusRatio, moveUpward.normalized, out hit, moveUpward.magnitude, groundMask))
+        if (Physics.CapsuleCast(p1, p2, radius * radiusRatio, moveUpward.normalized, out hit, castDistanceY, groundMask))
         {
             moveVec.y = 0;
         }
