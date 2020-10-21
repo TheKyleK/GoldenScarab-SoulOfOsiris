053246038eeeb2774f545177acd363d0b7487e74
Harry Fuge <harry.fuge@gmail.com>
2020-10-21 11:57:27 +1100

    += Beginning of Character Movement

---
+++ GoldenScarab-SoO/Assets/Scripts/CharacterRB.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CharacterRB : MonoBehaviour
+{
+
+    /// <summary>
+    /// public variables no hungarian notation
+    /// private/protected variables hungarian notation
+    /// </summary>
+
+
+    public Vector3 velocity;
+    public Vector3 acceleration;
+    public float friction;
+    public float maxSpeed;
+    public CharacterController controller;
+
+    void FixedUpdate()
+    {
+        velocity += acceleration * Time.fixedDeltaTime;
+        ClampMovement();
+        controller.Move(velocity * Time.fixedDeltaTime);
+        ApplyFriction();
+        acceleration *= 0;
+    }
+
+    void ClampMovement()
+    {
+        Vector3 clamp = new Vector3(velocity.x, 0, velocity.z);
+        if (clamp.magnitude > maxSpeed)
+        {
+            clamp = clamp.normalized * maxSpeed;
+            velocity.x = clamp.x;
+            velocity.z = clamp.z;
+
+        }
+    }
+
+    void ApplyFriction()
+    {
+        float decay = Mathf.Pow(1 - friction, Time.fixedDeltaTime);
+        velocity.x *= decay;
+        velocity.z *= decay;
+    }
+
+
+}
+
---
+++ GoldenScarab-SoO/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    CharacterRB m_rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rb = GetComponent<CharacterRB>();
+    }
+
+    // Update is called once per frame
+    void Update() 
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 movement = transform.forward * z + transform.right * x;
+        Vector3 direction = movement.normalized;
+        Vector3 force = direction * speed;
+
+        m_rb.acceleration += force;
+    }
+
+
+}
