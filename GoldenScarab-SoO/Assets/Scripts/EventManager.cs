f565baa6c01edcbb5b7b7b8c9be425c440519b65
Lucy <s200291@students.aie.edu.au>
2020-10-22 13:48:44 +1100

    + Event & Sound Manager
    Added resources

---
+++ GoldenScarab-SoO/Assets/Scripts/EventManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour
+{
+    public static EventManager current;
+
+    private void Awake()
+    {
+        current = this;
+    }
+
+    // Objects
+    public event Action<GameObject> onObjectTriggerEnter;
+    public void ObjectTriggerEnter(GameObject obj)
+    {
+        onObjectTriggerEnter?.Invoke(obj);
+    }
+
+    public event Action<GameObject> onObjectTriggerExit;
+    public void ObjectTriggerExit(GameObject obj)
+    {
+        onObjectTriggerExit?.Invoke(obj);
+    }
+}
---
+++ GoldenScarab-SoO/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Sound
+{
+    Chime,
+    Ding
+}
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager current;
+    [Range(0, 1)]
+    public float volume;
+
+    public Dictionary<Sound, AudioClip> audioMap = new Dictionary<Sound, AudioClip>();
+    private void Awake()
+    {
+        current = this;
+
+        audioMap.Add(Sound.Chime, Resources.Load<AudioClip>("Sounds/Chime"));
+        audioMap.Add(Sound.Ding, Resources.Load<AudioClip>("Sounds/Ding"));
+
+    }
+
+    public void PlaySound(Sound sound, Vector3 position)
+    {
+        AudioSource.PlayClipAtPoint(audioMap[sound], position, volume);
+    }
+}
