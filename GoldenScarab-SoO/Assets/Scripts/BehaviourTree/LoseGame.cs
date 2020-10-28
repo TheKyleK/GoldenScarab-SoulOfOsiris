e457061eec7c921bfd2f3e129710f7ee6bca24af
dannyli0109_ <dannyli0109@gmail.com>
2020-10-29 01:19:03 +1100

    + added win lose scenes

---
+++ GoldenScarab-SoO/Assets/Scripts/BehaviourTree/LoseGame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseGame : TreeNode
+{
+    public override BehaviourResult Execute(GameObject agent, Blackboard blackboard, float dt)
+    {
+        GameSceneManager.current.LoadScene(2, 1);
+        return BehaviourResult.Success;
+    }
+}
