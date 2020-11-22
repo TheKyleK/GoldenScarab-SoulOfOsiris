ab2cb9abcda60a8d4db34f80a4cc4d1b2e6190cd
Danny <dannyli0109@gmail.com>
2020-11-22 21:30:10 +1100

    + added almost all the lights, some textures, enable/disable lights when the player is too far away from it

---
+++ GoldenScarab-SoO/Assets/LightController.cs
@@ -0,0 +1,24 @@
+﻿using Boo.Lang.Environments;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    public Light myLight;
+
+    public void Start()
+    {
+        myLight.gameObject.SetActive(false);
+    }
+    // Start is called before the first frame update
+    public void Enable()
+    {
+        myLight.gameObject.SetActive(true);
+    }
+
+    private void Update()
+    {
+        myLight.gameObject.SetActive(false);
+    }
+}
