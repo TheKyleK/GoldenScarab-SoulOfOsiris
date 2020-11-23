43cf1e89eb670bb074df0be5827f9a5c5d146d67
Harrison McNeil <harrisonmcneildesign@gmail.com>
2020-11-23 16:21:05 +1100

    + temp folder, harrison v3

---
+++ GoldenScarab-SoO/Assets/DynamicLightSwitcher.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicLightSwitcher : MonoBehaviour
+{
+    public float radius;
+    public LayerMask mask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, radius, mask);
+        foreach (var hitCollider in hitColliders)
+        {
+            hitCollider.gameObject.GetComponent<LightController>().Enable();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, radius, mask);
+        foreach (var hitCollider in hitColliders)
+        {
+            hitCollider.gameObject.GetComponent<LightController>().Enable();
+        }
+    }
+}
