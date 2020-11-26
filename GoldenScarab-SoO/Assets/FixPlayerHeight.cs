a50a4deda4adab462cef998ea82917eff7c456b8
Danny <dannyli0109@gmail.com>
2020-11-26 16:01:04 +1100

    + commiting before sending the scene to the artists

---
+++ GoldenScarab-SoO/Assets/FixPlayerHeight.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixPlayerHeight : MonoBehaviour
+{
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(transform.position.x, 1, transform.position.z);
+    }
+}
