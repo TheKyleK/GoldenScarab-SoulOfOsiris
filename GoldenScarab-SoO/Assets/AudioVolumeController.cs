6ba38f520e362ac78a650e2462d3ced094a59e2b
Danny <dannyli0109@gmail.com>
2020-11-27 14:26:04 +1100

    = update nav mesh

---
+++ GoldenScarab-SoO/Assets/AudioVolumeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioVolumeController : MonoBehaviour
+{
+    public float volume;
+    public AudioSource audioSource;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        audioSource.volume = Setting.current.volume * volume;
+    }
+}
